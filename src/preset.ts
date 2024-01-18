4b0c3bc2974cf5f9c68999acf38a5f168da72bb4
Michael Shilman <shilman@users.noreply.github.com>
2024-01-18 21:15:05 +0800

    Restore presets

--- src/preset.ts
+++ src/preset.ts
@@ -1,8 +1,14 @@
 import { TransformOptions } from '@babel/core';
 import { ImportWriterPlugin } from './plugins/webpack-import-writer.js';
 import type { AddonOptions } from './types.js';
+import type { StorybookConfig } from '@storybook/types';
 import type { Options } from '@storybook/types';
 import type { Configuration } from 'webpack';
+
+export const managerEntries = (entry: string[] = []): string[] => [
+  ...entry,
+  require.resolve('./manager'),
+];
 
 export const babel = async (
   config: TransformOptions,
@@ -23,3 +29,8 @@ export async function webpackFinal(config: Configuration) {
   config.plugins = [...(config.plugins ?? []), new ImportWriterPlugin()];
   return config;
 }
+
+export const previewAnnotations: StorybookConfig['previewAnnotations'] = (entry = []) => [
+  ...entry,
+  require.resolve('./preview'),
+];
